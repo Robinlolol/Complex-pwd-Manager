e15bc746b0d27ffaf9079234e197e179f631fd2c
Robinlolol <105388497+Robinlolol@users.noreply.github.com>
2022-07-12 19:58:09 +0200

    Update t.py

--- PWD -manager Gui/t.py
+++ PWD -manager Gui/t.py
@@ -10,6 +10,8 @@ import collections, os
 import numpy as np
 from datetime import datetime
 
+#login Password = BKDF
+
 pos_num = ("1","2","3","4","5","6","7","8","9")
 pos_alp = tuple("a b c d e f g h i j k l m n o p q r s t u v w x y z".split())
 pos_sym = tuple("/ ( [ ) ] } ? ! - . ; :".split())
